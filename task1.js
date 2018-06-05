985685d70b0f5a6f388351bcaf3e2dcd9f9b2565
SERVER\TriZet <n-k86@mail.ru>
2018-06-05 22:06:29 +0300

    v4

--- task1.js
+++ task1.js
@@ -1,8 +1,17 @@
-﻿var n = Number(process.argv[2]);
+﻿function getPow(num, step)
+{
+    if(step <= 0)
+        return 1;
+    var pow = num;
+    while(--step)
+        pow *= num;
+    return pow;
+}
+var n = Number(process.argv[2]);
 var e = Number(process.argv[3]);
 var sum = 0;
 while(n) {
-    sum += Math.pow(n,e);
+    sum += getPow(n,e);
     n--;
 }
 console.log(sum);
