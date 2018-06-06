f1bfebcec7cd3d2033cd9488cb2445e14806f1ab
SERVER\TriZet <n-k86@mail.ru>
2018-06-06 15:09:32 +0300

    v5

--- task1.js
+++ task1.js
@@ -1,17 +1,8 @@
-﻿function getPow(num, step)
-{
-    if(step <= 0)
-        return 1;
-    var pow = num;
-    while(--step)
-        pow *= num;
-    return pow;
-}
-var n = Number(process.argv[2]);
+﻿var n = Number(process.argv[2]);
 var e = Number(process.argv[3]);
 var sum = 0;
 while(n) {
-    sum += getPow(n,e);
+    sum += Math.pow(n,e);
     n--;
 }
 console.log(sum);
--- task3.js
+++ task3.js
@@ -1,22 +1,17 @@
 ﻿function getText(val) {
     var val_div = val % 10;
-    if(val_div == 1)
+    if(val_div == 1 && val != 11)
         return 0;
-    else if( (val < 10 || val > 20) && (val_div >= 2 && val_div <= 4) )
+    else if(val_div >= 2 && val_div <= 4)
         return 1;
     else
         return 2;
-} 
-
+}
 var sec = Number(process.argv[2]) + Number(process.argv[3]);
 var h = sec / 3600 ^ 0;
 var m = (sec - h * 3600) / 60 ^ 0;
 var s = sec - h * 3600 - m * 60;
-
-var hour_text = ["час", "часа", "часов"];
-var min_text  = ["минута", "минуты", "минут"];
-var sec_text  = ["секунда", "секунды", "секунд"];
-
-var out_h = h ? (h + ' ' + hour_text[getText(h)] + ' ') : '';
-var out_m = m ? (m + ' ' + min_text[getText(m)]  + ' ') : '';
-console.log(out_h + out_m + s + ' ' + sec_text[getText(s)]);
+var hour_text = [" час ", " часа ", " часов "];
+var min_text  = [" минута ", " минуты ", " минут "];
+var sec_text  = [" секунда", " секунды", " секунд"];
+console.log((h?(h+hour_text[getText(h)]):'')+(m?(m+min_text[getText(m)]):'')+s+sec_text[getText(s)]);
